69a82f3937c29e3d798ea9d1a90107be2abf2bb7
bhavyar-806 <bhavyar806@gmai.com>
2025-06-22 12:51:12 +0530

    changes made

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "prod-rg"
-    storage_account_name = "aksbhavya15"
-    container_name       = "maale-123"
-    key                  = "prod.terraform.tfstate"
-  }
-}
-
