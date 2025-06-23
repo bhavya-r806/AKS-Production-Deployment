5567d04e349b237546d8a3799e0563621f791ccb
bhavyar-806 <bhavyar806@gmai.com>
2025-06-23 12:12:23 +0530

    removerd resource group in storagre

--- storage.tf
+++ storage.tf
@@ -13,6 +13,5 @@ resource "azurerm_storage_account" "storage_account" {
 resource "azurerm_storage_container" "storage_container" {
   name                  = "maale-123"
   storage_account_name  = azurerm_storage_account.storage_account.name
-  resource_group_name   = var.resource_group_name
   container_access_type = "private"
 }
